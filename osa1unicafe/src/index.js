56dd386eaf56cb1a00c01e35f07efe226160478e
Rene Trujillo <chedragon18@gmail.com>
2018-04-24 20:39:12 +0300

    Add task 1.7

--- osa1unicafe/src/index.js
+++ osa1unicafe/src/index.js
@@ -13,21 +13,42 @@ class Palautetta extends React.Component {
     }
   }
 
-  render() {
+  calculateKeskiarvo() {
+    let keskiarvo = (this.state.good-this.state.bad)/(this.state.good + this.state.neutral + this.state.bad);
 
+    if( isNaN(keskiarvo) ){
+      return 0;
+    } else {
+      return keskiarvo.toFixed(1);
+    }
+
+  }
+
+  calculatePositiivisia() {
+    let positiivisia = ((this.state.good)/(this.state.good + this.state.neutral + this.state.bad)) * 100;
+
+    if( isNaN(positiivisia) ){
+      return 0;
+    } else {
+      return positiivisia.toFixed(1);
+    }
+  }
+
+  render() {
     return(
       <div>
         <h2>anna palautetta</h2>
 
-        <button onClick={ () => this.setState({ good: this.state.good + 1 }) }>hyvä</button>
-        <button onClick={ () => this.setState({ neutral: this.state.neutral + 1 }) }>neutraali</button>
-        <button onClick={ () => this.setState({ bad: this.state.bad + 1 }) }>huono</button>
+        <button onClick={ () => this.setState({ good: this.state.good + 1}) }>hyvä</button>
+        <button onClick={ () => this.setState({ neutral: this.state.neutral + 1}) }>neutraali</button>
+        <button onClick={ () => this.setState({ bad: this.state.bad + 1}) }>huono</button>
 
         <h2>statistiikka</h2>
         hyvä: { this.state.good } <br/>
         neutraali { this.state.neutral }<br/>
         huono { this.state.bad }<br/>
-
+        keskiarvo { this.calculateKeskiarvo() }<br/>
+        positiivisia { this.calculatePositiivisia() } %<br/>
 
       </div>
     )
